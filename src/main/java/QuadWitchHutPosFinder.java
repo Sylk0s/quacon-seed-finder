3b1807a5269c8ded415248e0182174dcfec4d03b
Sylk0s <sylkos49@gmail.com>
2022-01-06 00:40:11 -0500

    initial commit, dumb code

---
+++ src/main/java/QuadWitchHutPosFinder.java
@@ -0,0 +1,65 @@
+import com.seedfinding.mcbiome.source.BiomeSource;
+import com.seedfinding.mcfeature.structure.RegionStructure;
+import com.seedfinding.mcfeature.structure.SwampHut;
+import com.seedfinding.mcmath.component.vector.QVector;
+import com.seedfinding.mccore.rand.ChunkRand;
+import com.seedfinding.mccore.rand.seed.RegionSeed;
+import com.seedfinding.mccore.util.pos.CPos;
+import com.seedfinding.mccore.util.pos.RPos;
+import com.seedfinding.mccore.version.MCVersion;
+import com.seedfinding.mcreversal.Lattice2D;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class QuadWitchHutPosFinder implements Runnable {
+    private static final long[] REGION_SEEDS = getQuadRegionSeeds();
+    public static final MCVersion VERSION = MCVersion.v1_17;
+    public static final SwampHut SWAMP_HUT= new SwampHut(VERSION);
+    private static final Lattice2D REGION_LATTICE = new Lattice2D(RegionSeed.A, RegionSeed.B, 1L << 48);
+
+    public static final RegionStructure<?, ?> CURRENT_STRUCTURE = SWAMP_HUT;
+    public static final int WORLD_SIZE=30_000_000;
+
+    @Override
+    public void run() {
+        long worldSeed = PigSpawnerFinder.seedlist.get(0);
+        PigSpawnerFinder.seedlist.remove(0);
+        int regionSize=CURRENT_STRUCTURE.getSpacing()*16;
+        int numberRegions=WORLD_SIZE/regionSize;
+        BiomeSource biomeSource = BiomeSource.of(CURRENT_STRUCTURE.getValidDimension(),VERSION, worldSeed);
+        for (long regionSeed:REGION_SEEDS){
+            for(QVector solution : REGION_LATTICE.findSolutionsInBox(regionSeed - worldSeed - CURRENT_STRUCTURE.getSalt(), -numberRegions, -numberRegions, numberRegions, numberRegions)) {
+                int regX=solution.get(0).intValue();
+                int regZ=solution.get(1).intValue();
+                if(!checkBiomes(biomeSource, regX,regZ, CURRENT_STRUCTURE)) continue;
+                System.out.println(new RPos(regX,regZ,regionSize).toBlockPos());
+            }
+        }
+    }
+
+    private static boolean checkBiomes(BiomeSource source, int regX, int regZ, RegionStructure<?,?> structure) {
+        if(checkStructure(source, regX,regZ, structure)) return false;
+        if(checkStructure(source,regX-1, regZ, structure)) return false;
+        if(checkStructure(source, regX, regZ-1, structure)) return false;
+        if(checkStructure(source, regX-1, regZ-1, structure)) return false;
+        return true;
+    }
+
+    private static boolean checkStructure(BiomeSource source, int regX, int regZ, RegionStructure<?,?> structure) {
+        CPos chunk = structure.getInRegion(source.getWorldSeed(), regX, regZ, new ChunkRand());
+        return !structure.canSpawn(chunk.getX(), chunk.getZ(), source);
+    }
+
+    private static long[] getQuadRegionSeeds() {
+        InputStream stream = PigSpawnerFinder.class.getResourceAsStream("/regionSeeds.txt");
+        return new BufferedReader(new InputStreamReader(Objects.requireNonNull(stream)))
+                .lines().mapToLong(Long::parseLong).toArray();
+    }
+
+    public static void main(String[] args) {
+        new QuadWitchHutPosFinder().run();
+    }
+}
